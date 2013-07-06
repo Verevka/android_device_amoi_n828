eb27a10b98bc9ef9c3be558af0c67236b5ab6c03
Jian He <hejian.he@gmail.com>
2013-07-06 18:29:41 +0800

    add recovery.fstab and init.rc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,9 +3,13 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/amoi/n828/BoardConfigVendor.mk
 
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
+TARGET_BOARD_PLATFORM := mt6589
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := n828
 
 BOARD_KERNEL_CMDLINE := 
@@ -21,6 +25,17 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/amoi/n828/kernel
 
-#BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/amoi/n828/recovery/recovery.fstab
+TARGET_RECOVERY_INITRC := device/amoi/n828/recovery/init.rc
+
+# Partition sizes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00600000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00600000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x28A00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x40000000
+BOARD_FLASH_BLOCK_SIZE := 131072
