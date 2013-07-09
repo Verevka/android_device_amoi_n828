35ee01bd0da0b47fd2cf76094155d7b88264ec46
Jian He <hejian.he@gmail.com>
2013-07-10 00:12:33 +0800

    fix mkbootimg

---
+++ CustomBootimg.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+MKBOOTIMG := device/amoi/n828/tools/mkbootimg
+
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
