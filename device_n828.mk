045dd8d402be59514508f78cbea1fb043a92d97a
Jian He <hejian.he@gmail.com>
2013-07-10 23:57:23 +0800

    fix CWM recovery

--- device_n828.mk
+++ device_n828.mk
@@ -17,6 +17,16 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+# recovery
+PRODUCT_COPY_FILES += \
+    device/amoi/n828/recovery/advanced_meta_init.rc:recovery/root/advanced_meta_init.rc \
+    device/amoi/n828/recovery/factory_init.project.rc:recovery/root/factory_init.project.rc \
+    device/amoi/n828/recovery/fstab:recovery/root/fstab \
+    device/amoi/n828/recovery/meta_init.modem.rc:recovery/root/meta_init.modem.rc \
+    device/amoi/n828/recovery/meta_init.project.rc:recovery/root/meta_init.project.rc \
+    device/amoi/n828/recovery/meta_init.rc:recovery/root/meta_init.rc \
+    device/amoi/n828/recovery/ueventd.rc:recovery/root/ueventd.rc
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
